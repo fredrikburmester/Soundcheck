7609617247956c3a0b6e7e3975a33d9afc481323
fredrikburmester <fredrik.burmester@gmail.com>
2021-04-07 10:23:20 +0200

    Added component

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h1>Login Page</h1>
+    <button v-on:click="loginWithSpotify">Log in with Spotify</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  components: {
+ 
+  },
+  methods: {
+      loginWithSpotify: function() {
+          console.log("[1]")
+          window.location.href = "https://accounts.spotify.com/authorize?client_id=bad02ecfaf4046638a1daa7f60cbe42b&response_type=code&redirect_uri=http://localhost:8080/logincallback&scope=user-read-private%20user-read-email&state=34fFs29kd09"
+      } 
+  }
+};
+</script>
---
+++ frontend/src/views/LoginCallback.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Nothing to see here
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginCallback",
+    methods: {
+        handleLoginAuthentication: function() {
+            console.log(this.$route.query.code)
+            localStorage.setItem('token', this.$route.query.code)
+            this.$router.push('/')
+        } 
+    },
+    beforeMount() {
+        this.handleLoginAuthentication()
+    },
+}
+</script>
