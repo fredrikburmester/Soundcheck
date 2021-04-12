62bc17945b607a41845e957fa871e5758274796b
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-12 17:13:12 +0200

    Added button component

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link :to="buttonLink">{{ buttonText }}</router-link>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    buttonLink: String,
+    buttonText: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
