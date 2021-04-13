12e44cda753fc05128fb5c142ffaa77374a0c66f
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-13 11:17:52 +0200

    styling for button

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-link :to="buttonLink">{{ buttonText }}</router-link>
+	<div class="button">
+  		<router-link :to="buttonLink">{{ buttonText }}</router-link>
+	</div>
 </template>
 
 <script>
@@ -14,5 +16,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 
+	.button {
+		font-size: 15px;
+		font-family: 'Roboto', sans-serif;
+		font-weight: 500;
+		letter-spacing: 2px;
+		background-color: green;
+		border-radius: 100px;
+		padding: 15px 25px 15px 25px;
+		width: 60vw;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.button a {
+		color: white;
+		text-transform: uppercase;
+		text-decoration: none;
+	}
 </style>
