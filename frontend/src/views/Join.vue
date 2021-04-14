4fed2c9684076cb3ee82edd7eb44a3a16af1352e
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-14 13:35:26 +0200

    created join field

--- frontend/src/views/Join.vue
+++ frontend/src/views/Join.vue
@@ -1,26 +1,53 @@
 <template>
 	<div class="join">
-		<h1>Join Room</h1>
-	    <router-link to="/">Home</router-link>
-        <input v-model="code" placeholder="Enter Room Code...">
-        <Button v-bind:buttonLink="code" buttonText="Join Room" />
+		<h1>Enter room code...</h1>
+        <!-- <input v-model="code" placeholder="Enter Room Code..."> -->
+        <InputField @input="updateCode" placeholder="ABCD" />
+        <Button :buttonLink="code_" buttonText="Join Room" />
+        <br>
+        <h1>Or...</h1>
+        <h1>Scan QR-code</h1>
+        <Button buttonLink="/" buttonText="Back" />
 	</div>
 </template>
 
 <script>
 import Button from '../components/Button'
+import InputField from '../components/InputField'
 
 export default {
 	name: "Join",
+    props: {
+        code: {
+            type: String,
+            default: '/'
+        }
+    },
     data() {
         return {
-            code: ''
+            code_: this.code
         }
     },
 	components: {
-        Button
+        Button,
+        InputField
 	},
 	methods: {
+        updateCode(value) {
+           console.log(value)
+           if(value.inputType == "deleteContentBackward") {
+               this.code_ = this.code_.slice(0, -1)
+           } else {
+               this.code_ += value.data
+           }
+           console.log(this.code_)
+        }
     }
 };
 </script>
+
+<style scoped>
+	.join {
+		margin-top:20vh;
+	}
+</style>
