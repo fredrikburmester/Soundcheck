b3d89e1e97c486847605ba9249742768b85dacad
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-20 17:17:24 +0200

    fix websockets for local dev

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,14 +4,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-console.log('pp ', process.env.NODE_ENV);
-
 var io = require('socket.io-client');
 var socket;
 
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'dev') {
+    console.log('Running in development mode');
     socket = io('http://localhost:5000');
 } else {
+    console.log('Running in production mode');
     socket = io('https://musicwithfriends.fdrive.se/', { path: '/ws' });
 }
 
