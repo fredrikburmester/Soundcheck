80918e144cc61678c95692ffcc7a54553c2e8894
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-23 17:02:25 +0200

    result page first commit

---
+++ frontend/src/views/Results.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Results</h1>
+        {{ code }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Results',
+    data: function() {
+        return {
+            code: this.$route.params.code
+        }
+    }
+}
+</script>
