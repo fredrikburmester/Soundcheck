467a7da8103f46e7be77c60673459b1ce256f1da
Daniel Hagstedt <danha896@student.liu.se>
2021-04-26 16:37:29 +0200

    Added state to track who has answered questions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,6 +4,7 @@ export default createStore({
     state: {
         roomCode: '',
         error: '',
+        players_guessed: [],
     },
     mutations: {
         updateRoomCode(state, value) {
@@ -11,6 +12,17 @@ export default createStore({
         },
         updateError(state, value) {
             state.error = value;
+        },
+        updatePlayersGuessed(state, value) {
+            for (let i of state.players_guessed) {
+                if (i == value) {
+                    return;
+                }
+            }
+            state.players_guessed.push(value.toString());
+        },
+        clearPlayersGuessed(state) {
+            state.players_guessed = [];
         },
     },
     actions: {},
