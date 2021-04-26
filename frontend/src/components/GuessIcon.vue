43aeddd162a5370789bcfa5f81de33bf1415167b
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-26 16:41:22 +0200

    new icon for guesses

---
+++ frontend/src/components/GuessIcon.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="playerAvatar">
+        <div>
+            <img class="circle">
+        </div>
+        <div class="name">
+            Hej
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerAvatar',
+    props: {
+        trackid: {
+            type: String,
+        },
+        guess: {
+            type: String,
+            default: '#FFF',
+        },
+        answer: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+</style>
