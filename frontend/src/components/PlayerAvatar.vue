18ec544df5135f86424823debe8a1042d1000e76
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-04-29 15:33:10 +0200

    fixed flex-start issue

--- frontend/src/components/PlayerAvatar.vue
+++ frontend/src/components/PlayerAvatar.vue
@@ -71,7 +71,7 @@ export default {
     margin-bottom: 10px;
     overflow-x: hidden;
     flex-wrap: nowrap;
-    justify-content: start;
+    justify-content: flex-start;
     align-items: center;
 }
 .circle {
