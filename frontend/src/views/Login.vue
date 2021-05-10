e4e0a1d49c3f84d0f43f3a980162a95fc362ecb2
Daniel Hagstedt <danha896@student.liu.se>
2021-05-10 18:42:35 +0200

    added scopes to read users existing playlists

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -29,7 +29,7 @@ export default {
     },
     methods: {
         loginWithSpotify: function () {
-            window.location.href = `https://accounts.spotify.com/authorize?client_id=bad02ecfaf4046638a1daa7f60cbe42b&response_type=code&redirect_uri=${process.env.VUE_APP_CALLBACK_URL}&scope=user-read-private%20user-top-read%20user-read-email%20playlist-modify-public%20playlist-modify-private&state=34fFs29kd09&show_dialog=true`;
+            window.location.href = `https://accounts.spotify.com/authorize?client_id=bad02ecfaf4046638a1daa7f60cbe42b&response_type=code&redirect_uri=${process.env.VUE_APP_CALLBACK_URL}&scope=user-read-private%20user-top-read%20user-read-email%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative&state=34fFs29kd09&show_dialog=true`;
         },
         checkToken: function () {
             if (localStorage.getItem('token')) {
