bee892b436379ba13b97938dbf635843f833e650
Daniel Hagstedt <danha896@student.liu.se>
2021-05-10 18:41:58 +0200

    added logic to track songs user wants to save

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,6 +7,8 @@ export default createStore({
         genre: false,
         no_songs: '1',
         time_range: 'short_term',
+        tracksForPlaylist: [],
+        nrOfTrackstoAdd: 0,
     },
     mutations: {
         updateRoomCode(state, value) {
@@ -15,6 +17,30 @@ export default createStore({
         updateError(state, value) {
             state.error = value;
         },
+        addTrack(state, value){
+            if(!state.tracksForPlaylist.includes(value)){
+                state.tracksForPlaylist.push([value, true]);
+                state.nrOfTrackstoAdd +=1;
+            }
+        },
+        //used to mark which tracks a user wants to save to a playlist
+        updateTracksForPlaylist(state, value){
+            if(state.tracksForPlaylist[value][1]){
+                state.tracksForPlaylist[value][1] = false;
+                state.nrOfTrackstoAdd -= 1;
+                console.log(state.nrOfTrackstoAdd)
+            }
+            else{
+                state.tracksForPlaylist[value][1] = true;
+                state.nrOfTrackstoAdd += 1;
+                console.log(state.nrOfTrackstoAdd)
+            }
+        },
+        //Needed to avoid tracks loading multiple times when reentering /playlist route from /results
+        clearTracksForPlaylist(state){
+            state.nrOfTrackstoAdd = 0;
+            state.tracksForPlaylist = [];
+        }
     },
     actions: {},
     modules: {},
