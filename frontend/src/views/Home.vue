751d5231dbbbd38299c688f25b91c070de2f1641
Josef Hamnert <josef.hamnert@gmail.com>
2021-05-31 12:40:50 +0200

    added error if user dont have any top tracks

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -18,6 +18,16 @@
                 button-text="my games"
                 color="#454545"
             />
+
+            <div v-if="error" class="errorpopup">
+                <div  class="noTopTrack"/>           
+                <div class="modal-view">
+                        <span class="close" @click="close()">&times;</span>
+                        <h3>Error!</h3>
+                        <p>No top tracks found</p>   
+                </div>
+            </div>
+            
             <p>{{ name }}</p>
             <div class="button-container">
                 <div class="logout">
@@ -47,7 +57,8 @@ export default {
     data: function () {
         return {
             name: '',
-            logo: logo
+            logo: logo,
+            error: this.$store.state.noTracksFound
         };
     },
     methods: {
@@ -71,6 +82,13 @@ export default {
             localStorage.removeItem('refresh_token');
             localStorage.removeItem('user_id');
             this.$router.push('/login');
+        },
+        close(){
+            this.error = false
+            self.$store.commit(
+                'updateNoTracks',
+                false
+            );
         },
     },
 };
@@ -101,5 +119,37 @@ export default {
     height: 2px;
     background-color: rgb(63, 63, 63);
     margin: 0 2rem 2rem 2rem;
+}
+.close {
+    color: #aaa;
+    font-size: 28px;
+    font-weight: bold;
+    position: absolute;
+    right: 20px;
+    cursor: pointer;
+}
+.noTopTrack{
+   width: 100vw;
+   height: 100vh;
+   opacity: 0.5;
+   background-color: #000;
+   position: fixed;
+   top: 0;
+   left: 0;
+   z-index: 3;
+}
+.modal-view{
+    z-index: 4;
+    position: absolute;
+    background-color: rgba(255, 255, 255);  
+    color: #CD1A2B;
+    top: 50%;
+    left: 50%;
+    width: 80vw;
+    height: 80px;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    padding: 2rem;
 }
 </style>
