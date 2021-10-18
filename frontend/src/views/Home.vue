28f6636d913534a51f64b52418f43e01a1c56d2c
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-10-18 11:24:10 +0200

    cleanup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -22,15 +22,6 @@ Home page with options for creating or joining a room and logging out.
                 button-text="my games"
                 color="#454545"
             />
-
-            <div v-if="error" class="errorpopup">
-                <div class="noTopTrack" />           
-                <div class="modal-view">
-                    <span class="close" @click="close()">&times;</span>
-                    <h3>Error!</h3>
-                    <p>No top tracks found</p>   
-                </div>
-            </div>
             <div class="button-container">
                 <div class="logout">
                     <Button
@@ -112,41 +103,5 @@ export default {
 .logout {
     padding: 0 2rem 0 2rem;
 }
-.hr {
-    height: 2px;
-    background-color: rgb(63, 63, 63);
-    margin: 0 2rem 2rem 2rem;
-}
-.close {
-    color: #aaa;
-    font-size: 28px;
-    font-weight: bold;
-    position: absolute;
-    right: 20px;
-    cursor: pointer;
-}
-.noTopTrack{
-   width: 100vw;
-   height: 100vh;
-   opacity: 0.5;
-   background-color: #000;
-   position: fixed;
-   top: 0;
-   left: 0;
-   z-index: 3;
-}
-.modal-view{
-    z-index: 4;
-    position: absolute;
-    background-color: rgba(255, 255, 255);  
-    color: #CD1A2B;
-    top: 50%;
-    left: 50%;
-    width: 80vw;
-    height: 80px;
-    -webkit-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    border-radius: 10px;
-    padding: 2rem;
-}
+
 </style>
